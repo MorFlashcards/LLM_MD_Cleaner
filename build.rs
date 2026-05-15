3e06f47d042ead0fc08d1df044bc16cabb360eeb
MorDictionary <mordictionary@gmail.com>
2026-05-14 22:06:07 -0400

    Add packaging and release assets

--- build.rs
+++ build.rs
@@ -1,3 +1,7 @@
 fn main() {
+    println!("cargo:rerun-if-changed=ui/app.slint");
+    println!("cargo:rerun-if-changed=assets/config/theme.toml");
+    println!("cargo:rerun-if-changed=assets/fonts/IMFellEnglish-Regular.ttf");
+
     slint_build::compile("ui/app.slint").expect("failed to compile Slint UI");
 }
